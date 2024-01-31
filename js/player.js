d8b74fce0c3ad9ca8f3cac0f6d59349313124a11
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-01-31 19:08:57 +0100

    fixed some issues

--- js/player.js
+++ js/player.js
@@ -100,7 +100,7 @@ class Player {
         if (this.health === 0) {
             alert(`Game Over! You scored ${this.score}`);
             location.reload();
-           // location.href = "game-over.html"  //game over screen
+         
         }
     }
     
